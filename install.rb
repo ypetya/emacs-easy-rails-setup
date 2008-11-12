cd286a0227a88ba1f50e78521a1e6522add2370d
Michael Pope <michael@dtcorp.com.au>
2008-11-12 16:15:56 +1100

    Uncommented out installation part

--- install.rb
+++ install.rb
@@ -6,9 +6,10 @@
 # Define variables
 userhome=ENV["HOME"]
 emacsfile = "#{userhome}/.emacs"
-emacsbak = "#{userhome}/.emacs.before_script"
+emacsfilebak = "#{userhome}/.emacs.before_script.bak"
+emacsdir = "#{userhome}/.emacs.d"
+emacsdirbak = "#{userhome}/.emacs.d.before_script.bak"
 
-=begin
 
 # Install Ports
 %w[emacs cedet-common cedet-contrib css-mode debian-el ecb ede emacs-goodies-el html-helper-mode ruby-elisp semantic speedbar yaml-mode].each do|port|
@@ -16,17 +17,26 @@ emacsbak = "#{userhome}/.emacs.before_script"
 	`sudo apt-get -y install #{port}`
 end
 
-=end
 
 puts "Copying emacs config file..."
 
 # Create first time backup only if backup doesn't exist
 if File.exists?(emacsfile)
-	if File.exists?(emacsbak)
+	if File.exists?(emacsfilebak)
 		puts "Backup exists"
 	else
-		puts "Creating first time backup #{emacsbak}"
-		`cp #{emacsfile} #{emacsbak}`
+		puts "Creating first time backup #{emacsfilebak}"
+		`cp #{emacsfile} #{emacsfilebak}`
+	end
+end
+
+# Create first time backup only if backup doesn't exist
+if File.exists?(emacsdir)
+	if File.exists?(emacsdirbak)
+		puts "Backup exists"
+	else
+		puts "Creating first time backup #{emacsdirbak}"
+		`cp -Rv #{emacsdir} #{emacsdirbak}`
 	end
 end
 
